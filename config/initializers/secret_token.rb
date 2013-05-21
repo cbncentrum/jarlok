8d72843490d958da3c9fa3d349c90c8d180bdccd
cbncentrum <cbncentrum@gmail.com>
2013-05-21 21:29:48 +0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jarlock::Application.config.secret_token = 'c375fceda351657759f8bb61378c73da207f148fb35a6211089cd7153edd72b06b0577858728f4cb80db1fe3014e689a2fb44fb018edf708953a313b0a1cbeb9'
